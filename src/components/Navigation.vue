2fb91764714216bc6cc17c0f48aec2bc583398e2
steamy <431917467@qq.com>
2017-11-08 21:15:37 +0800

    complete navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,58 +1,48 @@
 <template>
   <div class="navigation">
-    <nav class="navbar navbar-default" role="navigation">
-      <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">{{ msg }}</a>
-        </div>
 
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Link</a></li>
-            <li><a href="#">Link</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-              <ul class="dropdown-menu" role="menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li class="divider"></li>
-                <li><a href="#">Separated link</a></li>
-                <li class="divider"></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
-          </ul>
-          <form class="navbar-form navbar-left" role="search">
-            <div class="form-group">
-              <input type="text" class="form-control" placeholder="Search">
-            </div>
-            <button type="submit" class="btn btn-default">Submit</button>
-          </form>
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">Link</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-              <ul class="dropdown-menu" role="menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li class="divider"></li>
-                <li><a href="#">Separated link</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div><!-- /.navbar-collapse -->
-      </div><!-- /.container-fluid -->
-    </nav>
+      <header class="navbar navbar-static-top csl-header">
+
+        <div class="container">
+
+          <div class="navbar-header" style="margin-left: 80px">
+
+            <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#menu-navbar" aria-controls="ment-navbar" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+
+            <!--logo-->
+            <a class="navbar-brand" href="/">
+              <span><img src="../assets/img/logo/logo_3.png" class="lab-icon"/></span>
+              <span style="color: white;font-family: 方正兰亭黑简体;margin-left: 20px;font-size: xx-large">网络空降安全实验室</span>
+            </a>
+          </div> <!--navbar-header-->
+
+            <!--menu-->
+          <nav id="menu-navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav" style="margin-top: 25px;margin-left: 120px;">
+              <li>
+                <a href="/">学术</a>
+              </li>
+              <li>
+                <a href="/">团队</a>
+              </li>
+              <li>
+                <a href="/">合作</a>
+              </li>
+              <li>
+                <a href="/">通告</a>
+              </li>
+              <li>
+                <a href="/">博客</a>
+              </li>
+            </ul>
+          </nav>
+        </div><!-- /.container-fluid -->
+      </header>
   </div>
 </template>
 
@@ -69,4 +59,27 @@
 
 <style scoped>
 
+  .csl-header {
+    background-color: #003478 ;
+    border: none;
+    height: 100px;
+    -webkit-border-radius: 0;
+    -moz-border-radius: 0;
+    border-radius: 0;
+  }
+  .lab-icon {
+    width: 80px;
+    margin-top: -5px;
+  }
+
+  ul li{
+    margin-left: 30px;
+  }
+  ul li a:hover{
+    background-color: inherit;
+  }
+  ul li a{
+    color: white;
+    font-size: larger;
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // import Index from '@/components/Index'
 import Index from '@/components/Index'
+import Academy from '@/components/Academy'
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: Index
+    },
+    {
+      path: '/academy',
+      name: 'Academy',
+      component: Academy
     }
   ]
 })
