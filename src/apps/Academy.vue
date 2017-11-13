b5fb311fdf16eb9cb0502e55fe63cbbda9270776
steamy <431917467@qq.com>
2017-11-13 21:03:23 +0800

    add and config academy.html.

---
+++ src/apps/Academy.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="academy">
+    <navigation></navigation>
+  </div>
+</template>
+
+<script>
+  import Navigation from '../components/Navigation'
+  export default {
+    components: {Navigation},
+    name: 'Academy'
+  }
+</script>
+
+<style scoped>
+
+</style>
