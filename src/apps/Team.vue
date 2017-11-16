9d23b10fb85b81b28940419a9c61464295b930c8
steamy <431917467@qq.com>
2017-11-16 21:32:09 +0800

    team.html

--- src/apps/Team.vue
+++ src/apps/Team.vue
@@ -14,7 +14,7 @@
 
     <!--tab-->
     <div id="teamContain">
-      <div class="row" style="padding-top: 3em">
+      <div class="row" style="padding-top: 6em">
         <div class="col-md-2">
           <div class="line-flag"></div>
         </div>
@@ -23,27 +23,53 @@
         </div>
       </div>
 
-      <div class="teamTeaches">
-
+      <div class="teamTeaches" >
+        <div class="teacher row" style="padding-top: 10%;padding-bottom: 5%" v-for="teacher in teachers">
+          <div class="col-md-4">
+            <img style="width: 13em" :src="teacher.avatorUrl">
+          </div>
+          <div class="">
+            <p class="title-text">
+              {{teacher.name}}   <span style="color: #555555">{{teacher.professionalTitle}}</span>
+            </p>
+            <br/>
+            <p v-for="intro in teacher.simpleIntro">
+              {{intro}}
+            </p>
+          </div>
+        </div>
       </div>
     </div>
     <!--tab-->
-
-
+    <br/>
+    <br/>
+    <site-footer></site-footer>
 
   </div>
 </template>
 
 <script>
   import Navigation from '../components/Navigation'
+  import SiteFooter from '../components/SiteFooter'
   export default {
-    components: {Navigation},
+    components: {
+      SiteFooter,
+      Navigation},
     name: 'Team',
     data () {
       return {
       }
     },
     computed: {
+      teachers: function () {
+        var teachersJson = require('../assets/json/teachers.json')
+        console.log(teachersJson)
+        for (var i = 0; i < teachersJson.length; i++) {
+          var url = teachersJson[i].avatorUrl.split('/')
+          teachersJson[i].avatorUrl = require('../assets/img/teacher/' + url[url.length - 1])
+        }
+        return teachersJson
+      }
     },
     methods: {
     }
@@ -69,6 +95,7 @@
     min-height: 30em;
     background-color: white;
     border-radius: 0.3em;
+    padding-bottom: 5%;
   }
 
   .line-flag {
@@ -76,5 +103,11 @@
     margin-left: 6em;
     width: 0.4em;
     height: 4em;
+  }
+  p {
+    text-align: left;
+  }
+  .professional-tile {
+
   }
 </style>
