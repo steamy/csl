b017b04b92b5e3085e6af0498e585b48eca91e46
wangyu <609718145@qq.com>
2018-01-24 18:39:40 +0800

    修改合作

--- src/js/cooperation.js
+++ src/js/cooperation.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Cooperation from '../apps/Cooperation.vue'
+import router from '../router/cooperation'
 import 'jquery/dist/jquery.min'
 import '../assets/css/bootstrap/css/bootstrap.css'
 import '../assets/css/bootstrap/js/bootstrap.min'
@@ -9,5 +10,6 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#cooperation',
+  router,
   render: h => h(Cooperation)
 })
